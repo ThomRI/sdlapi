ee0dfafe585b5b5e4c698c6948aa880e3dbdb3b5
ThomRI <thomasrossi1@gmail.com>
2019-02-05 22:10:54 +0100

    Cluster base done

--- bin/Debug/client.py
+++ bin/Debug/client.py
@@ -8,32 +8,52 @@ Created on Tue Feb  5 19:30:36 2019
 import socket
 import struct
 import simulate
+import sys
+
+# Defines
+TRAME_VALUE_SIZE = 4
 
 # Commands
 SIMULATE = -1
 READY = -2
+DONE = -3
+
+EOF = -10
 
 HOST = '127.0.0.1'
 PORT = 65423
+MAX_SIMULATIONS_ALLOWED = 5
 
 def receive_cmd(socket):
-    cmd = struct.unpack('i', socket.recv(4))[0]
-    len_params = struct.unpack('i', socket.recv(4))[0]
+    cmd = struct.unpack('i', socket.recv(TRAME_VALUE_SIZE))[0]
+    len_params = struct.unpack('i', socket.recv(TRAME_VALUE_SIZE))[0]
     
     params = []
     for i in range(len_params):
-        params.append(struct.unpack('i', socket.recv(4))[0])
-        
+        params.append(struct.unpack('f', socket.recv(TRAME_VALUE_SIZE))[0])
+    
     return (cmd, params)
+
+def send(socket, cmd, parameters = []):
+    socket.send(struct.pack('i', cmd))
+    socket.send(struct.pack('i', len(parameters)))
+    
+    for p in parameters:
+        socket.send(struct.pack('f', float(p)))
+    
+    socket.send(struct.pack('i', EOF))
+    
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
     socket.connect((HOST, PORT))
-    socket.sendall(struct.pack('i', READY))
+    send(socket, READY, [MAX_SIMULATIONS_ALLOWED])
     
     (cmd, params) = receive_cmd(socket)
     if cmd == SIMULATE:
-        print("Starting", params[0], "simulations")
-        (average_x, average_y, average_dist, dev_percent_x, dev_percent_y, var_x, var_y) = simulate.simulate(params[0])
-        print(average_x, average_y, average_dist, dev_percent_x, dev_percent_y, var_x, var_y)
+        N = int(params[0])
+        print("Starting", N, "simulations")
+        (average_x, average_y, average_dist, dev_percent_x, dev_percent_y, var_x, var_y) = simulate.simulate(N)
+        print("Results (sent to the server) :", average_x, average_y, average_dist, dev_percent_x, dev_percent_y, var_x, var_y)
+        send(socket, DONE, [average_x, average_y, average_dist, dev_percent_x, dev_percent_y, var_x, var_y])
     
     socket.close()
--- bin/Debug/server.py
+++ bin/Debug/server.py
@@ -10,9 +10,14 @@ import selectors
 import types
 import struct
 
+# Defines
+TRAME_VALUE_SIZE = 4
+
 # Commands
 SIMULATE = -1
 READY = -2
+DONE = -3
+EOF = -10
 
 HOST = '127.0.0.1'
 PORT = 65423
@@ -33,10 +38,13 @@ def event_wrapper(key, mask):
     
     # Read
     if mask & selectors.EVENT_READ:
-        recv_data = clientsocket.recv(4) # Max 1024 bytes
+        recv_data = clientsocket.recv(TRAME_VALUE_SIZE)
 
         if recv_data: # Data received
-            event_read(clientsocket, recv_data, socketdata)
+            if len(recv_data) == TRAME_VALUE_SIZE and struct.unpack('i', recv_data)[0] == EOF: # End of a full trame
+                event_read(clientsocket, socketdata)
+            else: # Else required not to add the EOF
+                socketdata.inb += recv_data
         else: # Socket closed
             print("Client disconnected :", clientsocket.getsockname())
             sel.unregister(clientsocket)
@@ -47,17 +55,35 @@ def event_wrapper(key, mask):
             bytes_sent = clientsocket.send(socketdata.outb)
             socketdata.outb = socketdata.outb[bytes_sent:] # Next time we'll send the remaining datas
 
-def event_read(socket, recv_data, socketdata):
-    udata = struct.unpack('i', recv_data)[0] # Unpacked data to an integer
-    print("Received", udata, "from", socket.getsockname())
-    if udata == READY:
-        trigger_simulation(socket, socketdata)
+def event_read(socket, socketdata):
+    cmd = struct.unpack('i', socketdata.inb[:TRAME_VALUE_SIZE])[0]
+    len_params = struct.unpack('i', socketdata.inb[TRAME_VALUE_SIZE:2 * TRAME_VALUE_SIZE])[0]
+    socketdata.inb = socketdata.inb[2 * TRAME_VALUE_SIZE:]
+    params = []
+    for i in range(len_params):
+        params.append(struct.unpack('f', socketdata.inb[TRAME_VALUE_SIZE*i:TRAME_VALUE_SIZE*(i+1)])[0])
+    
+    socketdata.inb = socketdata.inb[len_params*TRAME_VALUE_SIZE:] # We read that already into params
+        
+    if cmd == READY:
+        # params[0] is the number of simulation max allowed
+        trigger_simulation(socket, socketdata, params[0])
+    elif cmd == DONE:
+        average_x = params[0]
+        average_y = params[1]
+        average_dist = params[2]
+        dev_percent_x = params[3]
+        dev_percent_y = params[4]
+        var_x = params[5]
+        var_y = params[6]
+        
+        print("Received simulation results :", average_x, average_y, average_dist, dev_percent_x, dev_percent_y, var_x, var_y)
         
 def send(socketdata, cmd, parameters):
     socketdata.outb += struct.pack('i', cmd)
     socketdata.outb += struct.pack('i', len(parameters)) # Number of parameters
     for p in parameters:
-        socketdata.outb += struct.pack('i', p)
+        socketdata.outb += struct.pack('f', p)
         
 def trigger_simulation(socket, socketdata, N):
     send(socketdata, SIMULATE, [N])
